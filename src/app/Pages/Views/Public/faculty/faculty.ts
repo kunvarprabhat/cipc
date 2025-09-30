e2c6af603ef136e455c6011a7e8ccbae23fb4541
HP <kunvarprabhat44@gmail.com>
2025-09-30 19:12:20 +0530

    message

---
+++ src/app/Pages/Views/Public/faculty/faculty.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faculty',
+  imports: [],
+  templateUrl: './faculty.html',
+  styleUrl: './faculty.css'
+})
+export class Faculty {
+
+}
